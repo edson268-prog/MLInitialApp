c7162952f0741b757cd7fff0c61344287b887bd5
edson.villacorta <edson26web@gmail.com>
2025-12-12 14:29:10 -0400

    First prediction

--- MLInitialApp.Console/Program.cs
+++ MLInitialApp.Console/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using MLInitialApp_Console;
+//Prompt user for input model from values
+
+//Build the input model from values
+PredictiveModel.ModelInput modelInput = new()
+{
+    UDI = 2F,
+    Product_ID = @"L47181",
+    Air_temperature = 298.2F,
+    Process_temperature = 308.7F,
+    Rotational_speed = 1408F,
+    Torque = 46.3F,
+    Tool_wear = 3F,
+};
+
+Console.WriteLine("ML.NET Predictive Model - Console App");
+
+// Passing input values into predictive model
+var scoresWithLabel = PredictiveModel.PredictAllLabels(modelInput);
+
+Console.WriteLine($"{"Class",-40}{"Score",-20}");
+Console.WriteLine($"{"-----",-40}{"-----",-20}");
+foreach (var score in scoresWithLabel)
+{
+    Console.WriteLine($"{score.Key,-40}{score.Value,-20}");
+}
+Console.WriteLine($"{"-----",-40}{"-----",-20}");
+
+var modelOutput = PredictiveModel.Predict(modelInput);
+Console.WriteLine($"Single Prediction Result");
+Console.WriteLine($"UDI {modelOutput.UDI}");
+Console.WriteLine($"Product_Id {modelOutput.Product_ID}");
+Console.WriteLine($"Air_temperature: {modelOutput.Air_temperature}");
+Console.WriteLine($"Process_temperature: {modelOutput.Process_temperature}");
+Console.WriteLine($"Rotational_speed: {modelOutput.Rotational_speed}");
+Console.WriteLine($"Torque: {modelOutput.Torque}");
+Console.WriteLine($"Tool_wear: {modelOutput.Tool_wear}");
+Console.WriteLine($"Machine_failure: {modelOutput.Machine_failure}");
+
+
+Console.WriteLine($"End of process, press any key to finish");
+Console.ReadKey();
